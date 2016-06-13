801c6a6621d2dc0376fc4aba81d76ecab8d923fe
lynnluo114 <lynn.luo1104@gmail.com>
2016-06-13 16:26:27 -0400

    Set up Solution DesignPatterns
    Simple implementation of Abstract Factory design pattern to support
    multiple database types (Oracle and SqlServer)

---
+++ AbstractFactory/OracleDatabase.cs
@@ -0,0 +1,49 @@
+﻿using Oracle.DataAccess.Client;
+using System.Data.Common;
+using System.Configuration;
+
+namespace AbstractFactory
+{
+    public class OracleDatabase : Database
+    {
+        private DbConnection _Connection = null;
+        private DbCommand _Command = null;
+
+        public override DbConnection Connection
+        {
+            get
+            {
+                if (_Connection == null)
+                {
+                    string connectionString = ConfigurationManager.ConnectionStrings["OracleConnectionString"].ConnectionString;
+                    _Connection = new OracleConnection(connectionString);
+                }
+                return _Connection;
+            }
+
+            set
+            {
+                _Connection = value;
+            }
+        }
+
+        public override DbCommand Command
+        {
+            get
+            {
+                if(_Command==null)
+                {
+                    _Command = new OracleCommand();
+                    _Command.Connection = Connection;
+                }
+                return _Command;
+            }
+
+            set
+            {
+                _Command = value;
+            }
+        }
+
+    }
+}
